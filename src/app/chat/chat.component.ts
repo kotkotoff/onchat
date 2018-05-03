bbe9aeb07dd5dba4c024500646bf67755654a67e
Denys.spirin <Denys.spirin@solarwinds.com>
2018-05-03 17:12:52 +0300

    work

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
+  animations: [
+    trigger('slideRight', [
+      transition('void <=> *', [
+        style([{ transform: 'translateX(-100%)' }]),
+        animate('1s ease-in', style({ transform: 'translateX(0)' }))
+      ])
+    ])
+  ],
   selector: 'chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
